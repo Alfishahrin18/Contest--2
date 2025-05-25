07587eb70418de58c1e0c11b7ac45fda67ea9e82
Alfi Shahrin <arikashahrin04.95@gmail.com>
2025-05-25 22:27:42 +0600

    (A) Timon and Pumbaa.cpp

---
+++ (A) Timon and Pumbaa.cpp
@@ -0,0 +1,17 @@
+1.// Problem name:  (A) Timon and Pumbaa.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 19/5/25
+
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main() {
+        int a, b;
+        cin >> a >> b;
+        
+        cout << max(a - b, 0) << endl;
+     
+        return 0;
+    }
